c86c5339c2a36771cb116e14bc14841e800b5a1d
romainChuat <romain.chuat@outlook.com>
2023-10-16 19:10:26 +0200

    add justifyText function

--- src/app.ts
+++ src/app.ts
@@ -2,9 +2,12 @@
 import express from 'express';
 import bodyParser from 'body-parser';
 import jwt from 'jsonwebtoken';
+import fs from 'fs';
+
+const lineWidth = 80;
 
 // Créez une instance de l'application Express
-const app = express();
+/*const app = express();
 
 // Configurez bodyParser pour analyser le JSON dans les requêtes
 app.use(bodyParser.json());
@@ -57,19 +60,15 @@ function createJustifyAPI(tokens: string[]){
     }
     if (!text) {
       return res.status(400).json({ message: 'Le texte est requis' });
-    }
+    }let tokens: string[] = [];
     console.log(tokens);
-    if(!tokens.includes(req.body.token)){
-      return res.status(400).json({ message: 'token invalide' });
-    }
 
     //justification du text        
-    
+    JustifyText(text);    
 
-    res.status(200).send(text);
+    res.status(200).send(text); 
   });
 
-  // Fonction pour justifier le texte
 
   // Port sur lequel l'application écoutera
   const port = 8080;
@@ -78,7 +77,62 @@ function createJustifyAPI(tokens: string[]){
   app.listen(port, () => {
     console.log(`Serveur en cours d'exécution sur le port ${port}`);
   });
-}
+}*/
 
-createToken();
-createJustifyAPI(tokens);
+var test = `Longtemps, je me suis couché de bonne heure. Parfois, à peine ma bougie éteinte, mes yeux se fermaient si vite que je n’avais pas le temps de me dire: «Je m’endors.» Et, une demi-heure après, la pensée qu’il était temps de chercher le sommeil m’éveillait; je voulais poser le volume que je croyais avoir dans les mains et souffler ma lumière; je n’avais pas cessé en dormant de faire des réflexions sur ce que je venais de lire, mais ces réflexions avaient pris un tour un peu particulier; il me semblait que j’étais moi-même ce dont parlait l’ouvrage: une église, un quatuor, la rivalité de François Ier et de Charles-Quint. 
+Cette croyance survivait pendant quelques secondes à mon réveil; elle ne choquait pas ma raison, mais pesait comme des écailles sur mes yeux et les empêchait de se rendre compte que le bougeoir n’était plus allumé. 
+ Puis elle commençait à me devenir inintelligible, comme après la métempsycose les pensées d’une existence antérieure; le sujet du livre se détachait de moi, j’étais libre de m’y appliquer ou non; aussitôt je recouvrais la vue et j’étais bien étonné de trouver autour de moi une obscurité, douce et reposante pour mes yeux, mais peut-être plus encore pour mon esprit, à qui elle apparaissait comme une chose sans cause, incompréhensible, comme une chose vraiment obscure. Je me demandais quelle heure il pouvait être; j’entendais le sifflement des trains qui, plus ou moins éloigné, comme le chant d’un oiseau dans une forêt, relevant les distances, me décrivait l’étendue de la campagne déserte où le voyageur se hâte vers la station prochaine; et le petit chemin qu’il suit va être gravé dans son souvenir par l’excitation qu’il doit à des lieux nouveaux, à des actes inaccoutumés, à la causerie récente et aux adieux sous la lampe étrangère qui le suivent encore dans le silence de la nuit, à la douceur prochaine du retour.`
+ 
+
+var justified = JustifyText(test, lineWidth);
+console.log(justified);
+
+
+fs.writeFile('justified_text.txt', justified, 'utf8', (err) => {
+  if (err) {
+    console.error('Erreur lors de l\'écriture du fichier :', err);
+  } else {
+    console.log('Texte justifié écrit dans le fichier justified_text.txt');
+  }
+});
+
+
+// A améliorer
+function JustifyText(text: string, lineWidth: number) {
+  const words = text.split(' ');
+  let lines: string[] = [''];
+  let current_line = '';
+
+  for (let j = 0; j < words.length; j++) {
+    if (current_line.length + words[j].length <= lineWidth) {
+      current_line += words[j] + ' ';
+    } else {
+      lines.push(current_line.trim());
+      current_line = words[j] + ' ';
+    }
+  }
+  lines.push(current_line);
+
+  //répartision des espaces
+  for (let i = 0; i < lines.length; i++) {
+    if (lines[i].length < lineWidth) {
+      let words_line = lines[i].split(' ');
+      let nb_gaps = words_line.length - 1;
+      let space_need = lineWidth - lines[i].length;
+
+      if (nb_gaps > 0) {
+        let extraSpaces = space_need % nb_gaps;
+        let spacesPerGap = Math.floor(space_need / nb_gaps);
+
+        for (let j = 0; j < nb_gaps; j++) {
+          words_line[j] += ' '.repeat(spacesPerGap);
+          if (j < extraSpaces) {
+            words_line[j] += ' ';
+          }
+        }
+        lines[i] = words_line.join(' ');
+      }
+    }
+  }
+  return lines.join('\n');
+}
